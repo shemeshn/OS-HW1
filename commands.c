f0d14829e60e853e8a6b5e67a5ba2366f7d01e19
Barelella <ellabarel1@gmail.com>
2018-11-21 13:09:24 +0200

    commit test

--- commands.c
+++ commands.c
@@ -11,7 +11,7 @@ int ExeCmd(void* jobs, char* lineSize, char* cmdString)
 {
 	char* cmd; 
 	char* args[MAX_ARG];
-	char pwd[MAX_LINE_SIZE];
+	char pwd[MAX_LINE_SIZE]; // Used in pwd command
 	char* delimiters = " \t\n";  
 	int i = 0, num_arg = 0;
 	bool illegal_cmd = FALSE; // illegal command
@@ -39,11 +39,12 @@ int ExeCmd(void* jobs, char* lineSize, char* cmdString)
 	/*************************************************/
 	else if (!strcmp(cmd, "pwd")) 
 	{
-		
+		getcwd(pwd, sizeof(pwd));
+        printf("%s\n", pwd);
 	}
 	
 	/*************************************************/
-	else if (!strcmp(cmd, "mkdir"))
+	else if (!strcmp(cmd, "history")) //Replaced mkdir' doesn't seem to be necessary.
 	{
  		
 	}
